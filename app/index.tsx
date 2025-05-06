cb130b4f75dca085e912e9fa9eed6e1c5aafc018
Abhik Baidya <abhikbaidya7@gmail.com>
2025-05-06 21:28:08 +0530

    Landing Page added

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import Button from "@/components/shared/Button";
+import Colors from "@/shared/Colors";
+import { Dimensions, Image, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require("../assets/images/landing.jpg")}
+        style={{
+          width: "100%",
+          height: Dimensions.get("screen").height,
+        }}
+      />
+      <View
+        style={{
+          position: "absolute",
+          height: Dimensions.get("screen").height,
+          backgroundColor: "#0707075e",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          padding: 20,
+        }}
+      >
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={{ height: 150, width: 150, marginTop: 150 }}
+        />
+        <Text
+          style={{
+            fontSize: 30,
+            color: Colors.WHITE,
+            fontWeight: "bold",
+          }}
+        >
+          AI Diet Planner
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            color: Colors.WHITE,
+            fontSize: 20,
+            marginTop: 15,
+            marginHorizontal: 20,
+            opacity: 0.8,
+          }}
+        >
+          Craft delicious , Healthy ,mean plans tailored just for you.Achieve
+          your goal with ease!
+        </Text>
+      </View>
+      <View
+        style={{ position: "absolute", bottom: 25, width: "100%", padding: 20 }}
+      >
+        <Button
+          title="Get Started"
+          onPress={() => console.log("Get Started Pressed")}
+        />
+        ;
+      </View>
     </View>
   );
 }
---
+++ components/shared/Button.tsx
@@ -0,0 +1,32 @@
+import Colors from "@/shared/Colors";
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+interface ButtonProps {
+  title: string;
+  onPress?: () => void;
+}
+
+export default function Button({ title, onPress }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        padding: 15,
+        backgroundColor: Colors.PRIMARY,
+        width: "100%",
+        borderRadius: 10,
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          color: Colors.WHITE,
+          textAlign: "center",
+        }}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}

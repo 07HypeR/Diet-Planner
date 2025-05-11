5b5440be57df7f2f60c5aeb7c513035ad5827b82
Abhik Baidya <abhikbaidya7@gmail.com>
2025-05-11 16:57:31 +0530

    Colour added

--- shared/Colors.tsx
+++ shared/Colors.tsx
@@ -1,7 +1,9 @@
 export default {
   WHITE: "#FFFFFF",
-  PRIMARY: "#8837ff",
+  PRIMARY: "#7e22ce",
+  SECONDARY: "#fbf5ff",
   GRAY: "gray",
   BLUE: "#4287f5",
   PINK: "#F542b3",
+  GREEN: "#39b981",
 };

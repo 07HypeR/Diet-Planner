e131ae94572b0a0d9341d4b2fb86e01c738036d1
Abhik Baidya <abhikbaidya7@gmail.com>
2025-05-11 16:57:13 +0530

    Complete recipe Prompt added

--- shared/Prompt.tsx
+++ shared/Prompt.tsx
@@ -6,4 +6,47 @@ export default {
        calories:<>,
        proteins:<>      
 }`,
+
+  GENERATE_RECIPE_OPTION_PROMPT: `Depends on user instruction create 3 different Recipe variant with Recipe Name with Emoji, 
+
+2 line description and main ingredient list in JSON format with field recipeName,description,ingredients (without size) only, Do not give me text response`,
+
+  GENERATE_COMPLETE_RECIPE_PROMPT: ` 
+
+  - As per recipeName and description give me recipeName and description as field,Give me all list of ingredients as ingredient ,
+
+  - emoji icons for each ingredient as icon, quantity as quantity, along with detail step by step recipe as steps
+
+  - Total Calories as calories (only number), Minutes to cook as cookTime and serving number as serveTo
+
+  - Total Proteins as proteins (only number)
+
+  - relastic image Text prompt as per reciepe as imagePrompt
+
+  - Give me category List for recipe from [Breakfast,Lunch,Dinner,Salad,Dessert,Fastfood,Drink,Cake] as category"
+
+  - Give me response in JSON format only
+
+  - Schema format should be:
+    {
+      "description":"string",
+      "recipeName":"string",
+      "calories":"string",
+      "proteins":"string",
+      "category":["string"],
+      "cookTime":"number",
+      "imagePrompt":"string",
+      "ingredients":[
+        {
+          "icon":"string",
+          "ingredient":"string",
+          "quantity":"string",
+        }
+      ],
+      "calories":<Single Person Serve>,
+      "proteins":<Single Person Serve>,
+      "serveTo":"number",
+      "steps":["string"]
+    }
+  `,
 };

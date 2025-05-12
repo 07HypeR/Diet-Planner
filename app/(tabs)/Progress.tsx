bf7d70dad1edd14f356fc681532ce87f6f3cbbc5
Abhik Baidya <abhikbaidya7@gmail.com>
2025-05-12 20:07:06 +0530

    Progress Screen Added

--- app/(tabs)/Progress.tsx
+++ app/(tabs)/Progress.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { Text, View } from "react-native";
+import GanerateRecipeCard from "@/components/home/GanerateRecipeCard";
+import TodayProgress from "@/components/home/TodayProgress";
+import TodaysMealPlan from "@/components/home/TodaysMealPlan";
+import React, { useState } from "react";
+import { FlatList, Platform, Text, View } from "react-native";
+import DateSelectionCard from "../../components/shared/DateSelectionCard";
 
 export default function Progress() {
+  const [selectedDate, setSelectedDate] = useState();
   return (
-    <View>
-      <Text>Progress</Text>
-    </View>
+    <FlatList
+      data={[]}
+      renderItem={() => null}
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={
+        <View
+          style={{
+            padding: 20,
+            paddingTop: Platform?.OS == "ios" ? 55 : 55,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 30,
+              fontWeight: "bold",
+            }}
+          >
+            Progress
+          </Text>
+          <DateSelectionCard setSelectedDate={setSelectedDate} />
+          <TodaysMealPlan selectedDate={selectedDate} />
+          <TodayProgress />
+          <GanerateRecipeCard />
+        </View>
+      }
+    />
   );
 }

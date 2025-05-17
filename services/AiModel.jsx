f5fca4d9a33f011d7974628855305cc85f0f93b2
Abhik Baidya <abhikbaidya7@gmail.com>
2025-05-17 16:04:37 +0530

    Gemini intigrated

--- services/AiModel.jsx
+++ services/AiModel.jsx
@@ -1,24 +1,38 @@
 import axios from "axios";
-import OpenAI from "openai";
 
-const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.EXPO_PUBLIC_OPENROUTER_API_KEY,
-});
+const GEMINI_API_URL =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent";
+const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
 
 export const CalculateCaloriesAi = async (PROMPT) =>
-  await openai.chat.completions.create({
-    model: "deepseek/deepseek-r1:free",
-    messages: [{ role: "user", content: PROMPT }],
-    response_format: "json_object",
-  });
+  await axios.post(
+    `${GEMINI_API_URL}?key=${API_KEY}`,
+    {
+      contents: [
+        {
+          parts: [{ text: PROMPT }],
+        },
+      ],
+    },
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 
 export const GenerateRecipe = async (PROMPT) =>
-  await openai.chat.completions.create({
-    model: "deepseek/deepseek-chat:free",
-    messages: [{ role: "user", content: PROMPT }],
-    response_format: "json_object",
-  });
+  await axios.post(
+    `${GEMINI_API_URL}?key=${API_KEY}`,
+    {
+      contents: [
+        {
+          parts: [{ text: PROMPT }],
+        },
+      ],
+    },
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 
 const BASE_URL = "https://diet-planner-image-server.onrender.com/api";
 export const RecipeImageApi = axios.create({
